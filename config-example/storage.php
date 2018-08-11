a2f7dd102053fb41a1f52a1f0874602bd298d783
Piepow <Piepow@users.noreply.github.com>
2018-08-11 12:17:15 -0400

    Forgot to make a config file use project path

--- config-example/storage.php
+++ config-example/storage.php
@@ -4,5 +4,5 @@ $projectConfig = include 'common/project.php';
 $projectPath = $projectConfig['path'];
 
 return [
-    'path' => '/home/plushie/Documents/web/ddnet-map-testing-log/storage/'
+    'path' => $projectPath . 'storage/'
 ];
